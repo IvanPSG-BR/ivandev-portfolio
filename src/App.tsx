375593b4ce72fa562d1c2ca7d744cc6ee8729004
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-08-18 01:13:45 -0300

    Refatoração da estrutura de componentes e adição de projetos
    - **Estrutura de Componentes e Layouts:**
      - Componentes reutilizáveis como `Button`, `Icon`, `ProjectCard`, `ScrollToTop`, `ThemeSwitcher` e `Title` foram criados e movidos para a pasta `src/components`.
      - A estrutura de layout foi estabelecida com a criação dos componentes `Header` e `Footer` em `src/layouts`.
      - O arquivo `main.tsx` foi configurado como o ponto de entrada da aplicação React, e o `index.html` foi criado como a página raiz.
    
    - **Configuração e Estilização:**
      - O `vite.config.ts` foi adicionado para configurar o ambiente de desenvolvimento com React e Tailwind CSS.
      - O `index.css` agora importa as diretivas do Tailwind CSS para estilização global.
      - As dependências do projeto em `package.json` e `package-lock.json` foram atualizadas para incluir as novas ferramentas e bibliotecas.
    
    - **Adição de Projetos:**
      - Foram adicionadas as páginas e estilos para diversos projetos, incluindo `CrazyTell`, `IMWA`, `JSONTables`, `OldWise` e `PrograMundo`, dentro da pasta `src/projects`.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="flex justify-center align-middle">
+            <p>Olá, Mundo!</p>
+        </div>
+    );
+}
